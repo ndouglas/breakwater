647cc0486871b0061d402aab8bf502af45c43f67
Nathan Douglas <github@darkdell.net>
2022-10-07 20:26:07 -0400

    Comments on unit conversion comments.

--- src/astronomy/math/unit/mass/mod.rs
+++ src/astronomy/math/unit/mass/mod.rs
@@ -1,9 +1,11 @@
 const KG_PER_SOLAR_MASS: f64 = 1.989E30;
 
+/// Msol -> KG
 pub fn msol_to_kg(msol: f64) -> f64 {
   msol * KG_PER_SOLAR_MASS
 }
 
+/// KG -> Msol
 pub fn kg_to_msol(kg: f64) -> f64 {
   kg / KG_PER_SOLAR_MASS
 }
