453483bdb3da08fe1d8b5efd9590b4509c55030d
Nathan Douglas <github@darkdell.net>
2022-10-07 20:23:34 -0400

    Rename a function or two.

--- src/astronomy/math/unit/density/mod.rs
+++ src/astronomy/math/unit/density/mod.rs
@@ -1,9 +1,11 @@
 const GRAMS_PER_CM3_PER_DSOL: f64 = 1.41;
 
+/// g/cm^3 -> Dsol
 pub fn grams_to_dsol(grams: f64) -> f64 {
   grams / GRAMS_PER_CM3_PER_DSOL
 }
 
+/// Dsol -> g/cm^3
 pub fn dsol_to_grams(dsol: f64) -> f64 {
   dsol * GRAMS_PER_CM3_PER_DSOL
 }
--- src/astronomy/math/unit/length/mod.rs
+++ src/astronomy/math/unit/length/mod.rs
@@ -1,11 +1,11 @@
 const METERS_PER_SOLAR_RADIUS: f64 = 149_597_870_700.0;
 const METERS_PER_AU: f64 = 1.496E11;
 
-pub fn solar_radii_to_meters(radii: f64) -> f64 {
+pub fn rsol_to_meters(radii: f64) -> f64 {
   radii * METERS_PER_SOLAR_RADIUS
 }
 
-pub fn meters_to_solar_radii(meters: f64) -> f64 {
+pub fn meters_to_rsol(meters: f64) -> f64 {
   meters / METERS_PER_SOLAR_RADIUS
 }
 
